c1f1c8baf36b117ac360f6d1af6c7fc18d73df85
nuhptr <nugrahaadi733@gmail.com>
2024-03-11 20:01:40 +0700

    model formatting

--- model/index.ts
+++ model/index.ts
@@ -1,6 +1,6 @@
 import * as z from "zod"
 
 export const LoginModel = z.object({
-   email: z.string().email({ message: "Email is required" }),
-   password: z.string().min(1, "Password at least have 1 character(s)"),
+    email: z.string().email({ message: "Email is required" }),
+    password: z.string().min(1, "Password at least have 1 character(s)"),
 })
