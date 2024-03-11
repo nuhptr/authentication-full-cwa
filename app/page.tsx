9cca7dd5bae6583b3bfb3d28c230ac0f3b9d0d7c
nuhptr <nugrahaadi733@gmail.com>
2024-03-11 18:58:01 +0700

    page update color paragraph

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
          <div className="space-y-6 text-center">
             <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md", font.className)}>🔐 Auth</h1>
-            <p>A simple authentication</p>
+            <p className="text-white">A simple authentication</p>
             <div>
                <LoginButton>
                   <Button variant="secondary" size="lg">
