5ad7773acd7730d3d24a93f3c71eaa9e5ec1310a
nuhptr <nugrahaadi733@gmail.com>
2024-03-12 14:51:42 +0700

    init prisma and lib

---
+++ lib/db.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client/extension"
+
+//* Extend the global object to include `prisma`
+declare global {
+    var prisma: PrismaClient | undefined
+}
+
+//* Create a new Prisma Client instance if in the first run
+export const db = globalThis.prisma || new PrismaClient()
+
+//* This is a workaround to avoid multiple instances of Prisma Client in development
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db
