5b0b9616cd363f1e6b83e51bad99b7314ec95896
nuhptr <nugrahaadi733@gmail.com>
2024-03-12 17:02:04 +0700

    udpate model

--- model/index.ts
+++ model/index.ts
@@ -4,3 +4,9 @@ export const LoginModel = z.object({
     email: z.string().email({ message: "Email is required" }),
     password: z.string().min(1, "Password at least have 1 character(s)"),
 })
+
+export const RegisterModel = z.object({
+    name: z.string().min(1, "Name at least have 1 character(s)"),
+    email: z.string().email({ message: "Email is required" }),
+    password: z.string().min(6, "Password at least have 6 character(s)"),
+})
