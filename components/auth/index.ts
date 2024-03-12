ee2efa52e60db376cdbd07f9b70b5e39c2078d9a
nuhptr <nugrahaadi733@gmail.com>
2024-03-12 17:02:18 +0700

    add main point auth component folder

---
+++ components/auth/index.ts
@@ -0,0 +1,9 @@
+import { BackButton } from "./back-button"
+import { CardWrapper } from "./card-wrapper"
+import { Header } from "./header"
+import { LoginButton } from "./login-button"
+import { LoginForm } from "./login-form"
+import { Social } from "./social"
+import { RegisterForm } from "./register-form"
+
+export { BackButton, CardWrapper, Header, LoginButton, LoginForm, Social, RegisterForm }
