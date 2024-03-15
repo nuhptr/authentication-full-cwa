a2c68de2eefebaed0ba8717d77262f59accc416c
nuhptr <nugrahaadi733@gmail.com>
2024-03-15 07:46:28 +0700

    auth setup: add some code

--- auth.ts
+++ auth.ts
@@ -1,10 +1,14 @@
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+
+import { db } from "@/lib/database"
+import authConfig from "@/auth.config"
 
 export const {
     handlers: { GET, POST },
     auth,
 } = NextAuth({
-    providers: [GitHub, Google],
+    adapter: PrismaAdapter(db),
+    session: { strategy: "jwt" },
+    ...authConfig,
 })
---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth"
+
+import authConfig from "@/auth.config"
+import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes } from "@/routes"
+
+const { auth } = NextAuth(authConfig)
+
+export default auth((req) => {
+    const { nextUrl } = req
+    const isLoggedIn = !!req.auth
+
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+})
+
+// Optionally, don't invoke Middleware on some paths
+// Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+export const config = {
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+}
