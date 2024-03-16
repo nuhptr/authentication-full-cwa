b37e32413ca119228b59655ddbee3b22b35098da
nuhptr <nugrahaadi733@gmail.com>
2024-03-16 22:49:39 +0700

    add middleware for gate app

--- middleware.ts
+++ middleware.ts
@@ -10,6 +10,23 @@ export default auth((req) => {
     const isLoggedIn = !!req.auth
 
     const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+
+    if (isApiAuthRoute) {
+        return
+    }
+
+    if (isAuthRoute) {
+        if (isLoggedIn) return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+        return
+    }
+
+    if (!isLoggedIn && !isPublicRoute) {
+        return Response.redirect(new URL("/auth/login", nextUrl))
+    }
+
+    return
 })
 
 // Optionally, don't invoke Middleware on some paths
