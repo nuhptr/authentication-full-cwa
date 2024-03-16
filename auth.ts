9e090f82c3fbc083791f45b98779137c7008b8d4
nuhptr <nugrahaadi733@gmail.com>
2024-03-16 22:49:28 +0700

    when add new property add also .d.ts

--- auth.ts
+++ auth.ts
@@ -1,13 +1,39 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import { UserRole } from "@prisma/client"
 
 import { db } from "@/lib/database"
 import authConfig from "@/auth.config"
+import { getUserById } from "@/data/user"
 
 export const {
     handlers: { GET, POST },
     auth,
+    signIn,
+    signOut,
 } = NextAuth({
+    callbacks: {
+        async session({ token, session }) {
+            if (token.sub && session.user) {
+                session.user.id = token.sub
+            }
+
+            if (token.role && session.user) {
+                session.user.role = token.role as UserRole
+            }
+
+            return session
+        },
+        async jwt({ token }) {
+            if (!token.sub) return token
+
+            const existingUser = await getUserById(token.sub) // token.sub is the user id
+            if (!existingUser) return token
+            token.role = existingUser.role
+
+            return token
+        },
+    },
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt" },
     ...authConfig,
