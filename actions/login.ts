4cf2600d054972e8bdbfaaa7ffcd520d8202734f
nuhptr <nugrahaadi733@gmail.com>
2024-03-16 22:40:36 +0700

    actions: login update logic signIn with auth package

--- actions/login.ts
+++ actions/login.ts
@@ -1,13 +1,37 @@
 "use server"
 
 import * as z from "zod"
+import { AuthError } from "next-auth"
 
+import { signIn } from "@/auth"
 import { LoginModel } from "@/model"
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
 
 export const login = async (values: z.infer<typeof LoginModel>) => {
     const validateFields = LoginModel.safeParse(values)
 
-    if (!validateFields.success) return { errors: "Invalid fields!" }
+    if (!validateFields.success) return { error: "Invalid fields!" }
 
-    return { success: "Email sent!" }
+    const { email, password } = validateFields.data
+
+    try {
+        await signIn("credentials", {
+            email,
+            password,
+            redirectTo: DEFAULT_LOGIN_REDIRECT,
+        })
+
+        return { success: "Email sent!" }
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case "CredentialsSignin":
+                    return { error: "Invalid credentials!" }
+                default:
+                    return { error: "Something went wrong!" }
+            }
+        }
+
+        throw error
+    }
 }
