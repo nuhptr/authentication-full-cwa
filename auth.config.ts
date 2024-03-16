e4f4b1791a2aebb2d266e44448f8cb4d95770598
nuhptr <nugrahaadi733@gmail.com>
2024-03-16 22:47:06 +0700

    add clientId and clientSecret

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,39 @@
 import type { NextAuthConfig } from "next-auth"
+import credentials from "next-auth/providers/credentials"
+import bcryptjs from "bcryptjs"
 
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
+import github from "next-auth/providers/github"
+import google from "next-auth/providers/google"
+
+import { LoginModel } from "@/model"
+import { getUserByEmail } from "@/data/user"
 
 export default {
-    providers: [GitHub, Google],
+    providers: [
+        google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+        github({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        }),
+        credentials({
+            async authorize(credentials) {
+                const validateFields = LoginModel.safeParse(credentials)
+
+                if (validateFields.success) {
+                    const { email, password } = validateFields.data
+
+                    const user = await getUserByEmail(email)
+                    if (!user || !user.password) return null
+
+                    const passwordMatch = await bcryptjs.compare(password, user.password)
+                    if (passwordMatch) return user
+                }
+
+                return null
+            },
+        }),
+    ],
 } satisfies NextAuthConfig
