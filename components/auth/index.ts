4f2c65a7dae4ba1d2606ab8ce05c57eeabb73667
nuhptr <nugrahaadi733@gmail.com>
2024-03-24 00:02:08 +0700

    index/add error-card and new-verification-form component

--- components/auth/index.ts
+++ components/auth/index.ts
@@ -5,5 +5,17 @@ import { LoginButton } from "./login-button"
 import { LoginForm } from "./login-form"
 import { Social } from "./social"
 import { RegisterForm } from "./register-form"
+import { ErrorCard } from "./error-card"
+import { NewVerificationForm } from "./new-verification-form"
 
-export { BackButton, CardWrapper, Header, LoginButton, LoginForm, Social, RegisterForm }
+export {
+    BackButton,
+    CardWrapper,
+    Header,
+    LoginButton,
+    LoginForm,
+    Social,
+    RegisterForm,
+    ErrorCard,
+    NewVerificationForm,
+}
