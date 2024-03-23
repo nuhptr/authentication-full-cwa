2dfdfd999f344cdbcceae42849083787935b186b
nuhptr <nugrahaadi733@gmail.com>
2024-03-24 00:03:58 +0700

    login-form/add error based on search params

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,8 +1,9 @@
 "use client"
 
-import React, { useState, useTransition } from "react"
 import * as z from "zod"
 import { useForm } from "react-hook-form"
+import React, { useState, useTransition } from "react"
+import { useSearchParams } from "next/navigation"
 import { zodResolver } from "@hookform/resolvers/zod"
 
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
@@ -17,6 +18,12 @@ import { LoginModel } from "@/model"
 import { login } from "@/actions/login"
 
 export const LoginForm = () => {
+    const searchParams = useSearchParams()
+    const urlError =
+        searchParams.get("error") === "OAuthAccountNotLinked"
+            ? "Email already in use with different provider"
+            : ""
+
     const [error, setError] = useState<string | undefined>("")
     const [success, setSuccess] = useState<string | undefined>("")
     const [isPending, startTransition] = useTransition()
@@ -35,8 +42,8 @@ export const LoginForm = () => {
 
         startTransition(() => {
             login(values).then((data) => {
-                setError(data.error)
-                setSuccess(data.success)
+                setError(data?.error)
+                setSuccess(data?.success) // NOTE: add when we add 2FA
             })
         })
 
@@ -72,7 +79,7 @@ export const LoginForm = () => {
                             disabled={isPending}
                             render={({ field }) => (
                                 <FormItem>
-                                    <FormLabel htmlFor="email">Email</FormLabel>
+                                    <FormLabel htmlFor="password">Password</FormLabel>
                                     <FormControl>
                                         <Input {...field} placeholder="*******" type="password" />
                                     </FormControl>
@@ -81,7 +88,7 @@ export const LoginForm = () => {
                             )}
                         />
                     </div>
-                    <FormError message={error} />
+                    <FormError message={error || urlError} />
                     <FormSuccess message={success} />
                     <Button type="submit" className="w-full" disabled={isPending}>
                         Login
