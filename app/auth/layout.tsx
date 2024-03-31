4409d4d3cd4fde5747747dab9960889637eb3fc0
nuhptr <nugrahaadi733@gmail.com>
2024-03-31 10:40:05 +0700

    app/auth: add layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,9 +1,7 @@
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex items-center justify-center h-full bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+        <div className="flex items-center justify-center h-full bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-emerald-400 to-green-800">
             {children}
         </div>
     )
 }
-
-export default AuthLayout
