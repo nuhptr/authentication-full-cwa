1c70d5c54b22c494ba5365460945f470187a8d3f
nuhptr <nugrahaadi733@gmail.com>
2024-03-31 10:34:09 +0700

    app: change className and add loginButton asChild

--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,20 @@ import { Button } from "@/components/ui/button"
 import { LoginButton } from "@/components/auth"
 
 const font = Poppins({ subsets: ["latin"], weight: ["600"] })
+const headerClass = "text-6xl font-semibold text-white drop-shadow-md"
+
+const mainClass =
+    "flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-emerald-400 to-green-800"
 
 export default function Home() {
     return (
-        <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-            <div className="space-y-6 text-center">
-                <h1
-                    className={cn(
-                        "text-6xl underline font-semibold text-white drop-shadow-md",
-                        font.className
-                    )}>
-                    🔐 Auth
-                </h1>
+        <main className={mainClass}>
+            <div className="text-center space-y-6">
+                <h1 className={cn(headerClass, font.className)}>🔐 Auth</h1>
                 <p className="text-white">A simple authentication</p>
+
                 <div>
-                    <LoginButton>
+                    <LoginButton asChild>
                         <Button variant="secondary" size="lg">
                             Sign in
                         </Button>
