0fb6d73df6b116dc028bd4569203898d13f12663
nuhptr <nugrahaadi733@gmail.com>
2024-03-31 10:38:27 +0700

    app/(protected):client page

---
+++ app/(protected)/client/page.tsx
@@ -0,0 +1,13 @@
+import { UserInfo } from "@/components/user-info"
+
+import { useCurrentUser } from "@/hooks/use-current-user"
+
+export default async function ClientPage() {
+    const user = useCurrentUser()
+
+    return (
+        <div>
+            <UserInfo label="✨ Client Component" user={user} />
+        </div>
+    )
+}
