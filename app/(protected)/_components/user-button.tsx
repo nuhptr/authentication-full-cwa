ca72651a25e77c34c871cb8a2b997306ffaccba4
nuhptr <nugrahaadi733@gmail.com>
2024-03-31 10:37:48 +0700

    app/(protected):user-button add new code for logout action

---
+++ app/(protected)/_components/user-button.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { FaUser } from "react-icons/fa"
+import { ExitIcon } from "@radix-ui/react-icons"
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
+import { LogoutButton } from "@/components/auth/logout-button"
+
+import { useCurrentUser } from "@/hooks/use-current-user"
+
+export function UserButton() {
+    const user = useCurrentUser()
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Avatar>
+                    <AvatarImage src={user?.image || ""} />
+                    <AvatarFallback className="bg-sky-500">
+                        <FaUser className="text-white" />
+                    </AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent className="w-40" align="end">
+                <LogoutButton>
+                    <DropdownMenuItem>
+                        <ExitIcon className="w-4 h-4 mr-2" />
+                        Log Out
+                    </DropdownMenuItem>
+                </LogoutButton>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
