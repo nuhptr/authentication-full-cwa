7dceb9b16d844546cc00ad0821b3c549d7a85396
nuhptr <nugrahaadi733@gmail.com>
2024-03-31 09:10:49 +0700

    actions: register update code

--- actions/register.ts
+++ actions/register.ts
@@ -3,37 +3,35 @@
 import * as z from "zod"
 import bcrypt from "bcryptjs"
 
-import { RegisterModel } from "@/model"
+import { RegisterModel } from "@/model/auth-model"
 import { getUserByEmail } from "@/data/user"
+
+import { generateVerificationToken } from "@/lib/tokens"
 import { sendVerificationEmail } from "@/lib/mail"
-import { generateVerificationToken } from "@/data/tokens"
-
 import { db } from "@/lib/database"
-import { logger } from "@/lib/logging"
 
-export const register = async (values: z.infer<typeof RegisterModel>) => {
+export async function register(values: z.infer<typeof RegisterModel>) {
     const validateFields = RegisterModel.safeParse(values)
+
     if (!validateFields.success) {
         return { error: "Invalid fields!" }
     }
 
     const { email, password, name } = validateFields.data
+
     const hashedPassword = await bcrypt.hash(password, 10)
 
     const existingUser = await getUserByEmail(email)
+
     if (existingUser) {
         return { error: "User already exists!" }
     }
 
     const response = await db.user.create({
-        data: {
-            name,
-            email,
-            password: hashedPassword,
-        },
+        data: { name, email, password: hashedPassword },
     })
 
-    logger.info(`data: ${JSON.stringify(response)}`)
+    console.info("User created:", response)
 
     const verificationToken = await generateVerificationToken(email)
     await sendVerificationEmail(verificationToken.email, verificationToken.token)
