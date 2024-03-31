3717066cf350b57e9cf970a7bd62747403275614
nuhptr <nugrahaadi733@gmail.com>
2024-03-31 10:36:35 +0700

    app/(protected):add layout for addd navbar

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "@/app/(protected)/_components/navbar"
+
+type Props = {
+    children: React.ReactNode
+}
+
+export default function ProtectedLayout({ children }: Props) {
+    return (
+        <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-emerald-400 to-green-800">
+            <Navbar />
+            {children}
+        </div>
+    )
+}
