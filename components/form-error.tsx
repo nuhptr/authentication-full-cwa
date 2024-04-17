70096337b3e98e13ce131d1dcb5389e45283d78e
nuhptr <nugrahaadi733@gmail.com>
2024-04-17 21:37:31 +0700

    components: formatting code

--- components/form-error.tsx
+++ components/form-error.tsx
@@ -1,17 +1,17 @@
 import React from "react"
 import { ExclamationTriangleIcon } from "@radix-ui/react-icons"
 
-type FormErrorProps = {
-    message?: string
+interface FormErrorProps {
+   message?: string
 }
 
 export function FormError({ message }: FormErrorProps) {
-    if (!message) return null
+   if (!message) return null
 
-    return (
-        <div className="flex items-center p-3 text-sm bg-destructive/15 rounded-md gap-x-2 text-destructive">
-            <ExclamationTriangleIcon className="w-4 h-4" />
-            <p className="">{message}</p>
-        </div>
-    )
+   return (
+      <div className="flex items-center p-3 text-sm bg-destructive/15 rounded-md gap-x-2 text-destructive">
+         <ExclamationTriangleIcon className="w-4 h-4" />
+         <p className="">{message}</p>
+      </div>
+   )
 }
--- components/form-success.tsx
+++ components/form-success.tsx
@@ -1,17 +1,17 @@
 import React from "react"
 import { CheckCircledIcon } from "@radix-ui/react-icons"
 
-type FormErrorProps = {
-    message?: string
+interface FormErrorProps {
+   message?: string
 }
 
 export function FormSuccess({ message }: FormErrorProps) {
-    if (!message) return null
+   if (!message) return null
 
-    return (
-        <div className="flex items-center p-3 text-sm bg-emerald-500/15 rounded-md gap-x-2 text-emerald-500">
-            <CheckCircledIcon className="w-4 h-4" />
-            <p className="">{message}</p>
-        </div>
-    )
+   return (
+      <div className="flex items-center p-3 text-sm bg-emerald-500/15 rounded-md gap-x-2 text-emerald-500">
+         <CheckCircledIcon className="w-4 h-4" />
+         <p className="">{message}</p>
+      </div>
+   )
 }
